3d16192e307294e169bbb9917c863ad8577b053b
AirOne01 <popgthyrd@gmail.com>
2022-10-28 20:40:07 +0200

    feat(log): add full logging support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use colored::Colorize;
 use dialoguer::{theme::ColorfulTheme, Confirm};
 use std::process::Command;
+use std::str::from_utf8;
 
 fn main() {
     if Confirm::with_theme(&ColorfulTheme::default())
@@ -7,16 +9,32 @@ fn main() {
         .interact()
         .unwrap()
     {
-        if cfg!(target_os = "windows") {
+        let res = if cfg!(target_os = "windows") {
             Command::new("cmd")
                 .args(["/C", "runas /usr:administrator /savecred \"shutdown /h\""])
                 .output()
-                .expect("Failed to elevate permission and execute the process");
+                .expect("Failed to elevate permission and execute the process")
         } else {
             Command::new("sh")
                 .args(["-c", "sudo systemctl hibernate"])
                 .output()
-                .expect("Failed to execute the process");
-        }
+                .expect("Failed to execute the process")
+        };
+        if !res.status.success() {
+            println!("status: {}", res.status.to_string().red());
+        };
+        if !res.stdout.is_empty() {
+            println!("stdout: {}", match_u8(&res.stdout[..]));
+        };
+        if !res.stderr.is_empty() {
+            println!("stderr: {}", match_u8(&res.stderr[..]).red());
+        };
+    }
+}
+
+fn match_u8(output: &[u8]) -> String {
+    match from_utf8(output) {
+        Ok(v) => format!("\n{}", v),
+        Err(_) => "invalid UTF-8 sequence".red().to_string(),
     }
 }
